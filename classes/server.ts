6c76344b0c817a61b75016c6870670233d3361b8
DJR <djr@gmail.com>
2023-03-21 17:43:36 -0600

    Se impplementa patron singleton para evitar la creacion de instancias del server no controladas

--- classes/server.ts
+++ classes/server.ts
@@ -1,13 +1,32 @@
 import express from 'express';
 import { SERVER_PORT } from '../global/environment';
+import socketIO from 'socket.io';
+import http from 'http';
 
 export default class Server {
     public app: express.Application;
     public port: number;
+    public io: socketIO.Server;
+    private httpServer: http.Server;
+    private static _instance: Server;
 
-    constructor() {
+    private constructor() {        
         this.app = express();
         this.port = SERVER_PORT;
+        this.httpServer = new http.Server(this.app);
+        this.io = new socketIO.Server( this.httpServer, { cors: { origin: true, credentials: true } } );
+        this.escucharSockets();
+    }
+
+    public static get instance() {
+        return this._instance || (this._instance = new this());
+    }
+
+    escucharSockets() {
+        this.io.on('connection', () => {
+            console.log('Listening sockets...')
+            console.log('New client connected');
+        });
     }
 
     start(callback: any) {
