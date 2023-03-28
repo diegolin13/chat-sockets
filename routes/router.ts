8ce50ff1efbe964afd0a37dd8084d97a02dba597
DJR <djr@gmail.com>
2023-03-27 19:47:43 -0600

    mensajes en servicios rest

--- routes/router.ts
+++ routes/router.ts
@@ -1,4 +1,5 @@
 import { Request, Response, Router } from "express";
+import Server from "../classes/server";
 
 const router = Router();
 
@@ -14,11 +15,37 @@ router.get('/mensajes', (req: Request, res: Response) => {
 router.post('/mensajes/:id', (req: Request, res: Response) => {
     const {body, from} = req.body;
     const id = req.params.id;
+
+    const payload = {
+        from,
+        body
+    }
+
+    const server = Server.instance;
+
+    server.io.in(id).emit('mensaje-privado', payload);
     res.json({
         ok: true,
         body,
         from,
         id
+    });
+});
+
+
+router.post('/mensajes', (req: Request, res: Response) => {
+    const {body, from} = req.body;
+    const server = Server.instance;
+    const payload = {
+        from,
+        body
+    }
+
+    server.io.emit('mensaje-nuevo', payload);
+    res.json({
+        ok: true,
+        body,
+        from
     });
 });
 
